208774bfe2ced71d8b0e131f8bf1c69696eddf2d
kanai1 <ksh30918@kakao.com>
2023-11-12 13:07:44 +0900

    Feat: 게시판 추가에 따른 라우팅

--- server/Router/board/router.js
+++ server/Router/board/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const board = require('../../api/board')
 const router = express.Router()
 
-router.get('/list', board.list)
+router.get('/list/:boardname', board.list)
 router.post('/post', board.post)
 router.get('/post/:num', board.get)
 
