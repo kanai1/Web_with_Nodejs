18c8a4115501e3ea8e2871a28b2051aa13476f53
kanai1 <ksh30918@kakao.com>
2023-11-12 13:10:23 +0900

    Rename: Mypage -> Header - Header라는 이름이 더 맞는거같음

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export {default as Main} from './Main';
 export {default as Login} from './Login';
-export {default as Mypage} from './Mypage';
+export {default as Mypage} from './Header';
 export {default as Register} from './Register';
 export {default as Board} from './Board';
 export {default as Post} from './Post';
